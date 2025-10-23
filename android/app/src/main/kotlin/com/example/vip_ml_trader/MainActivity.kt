bc93428636fac6f81510b7b28991fcff0d2fee21
اسمك <@.com>
2025-10-23 22:33:13 +0200

    fix Android embedding v2

--- android/app/src/main/kotlin/com/example/vip_ml_trader/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vip_ml_trader/MainActivity.kt
@@ -1,6 +1,1 @@
-package com.example.vip_ml_trader
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+package com.example.vip_ml_trader\n\nimport io.flutter.embedding.android.FlutterActivity\n\nclass MainActivity: FlutterActivity() {}
